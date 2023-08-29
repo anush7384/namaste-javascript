80439e63bbbd57d9ccecc691c7818d7adce8a886
Anush Rawat <anushra0701@gmail.com>
2023-08-29 20:22:24 +0530

    not defined vs undefined

---
+++ undefined/script.js
@@ -0,0 +1,18 @@
+// during memory allocation phase a is alloted memory and for placeholder it is assigned undefined
+console.log(a); //undefined   
+
+var a = 10;
+
+// console.log(x);
+// here the console will be not defined as it has not been defined
+// it is different that undefined 
+// a has been assigned in memory whereas x is not present inside execution context
+
+// javascript is a loosely typed language it doesnot attach a data type to variable
+var x = 10; // x is storing number
+x = "anush"// now x is storing string
+console.log(x); // output is anush
+
+var b = undefined; // not a good way though it is completely okay
+// as undefined means that during program it was not assigned any value
+
