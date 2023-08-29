2d70372b33374c40a7947bdcd1571353316dc744
Anush Rawat <anushra0701@gmail.com>
2023-08-29 12:51:25 +0530

    Execution context notes and code added

---
+++ Execution Context/execution.js
@@ -0,0 +1,11 @@
+var n = 2;
+function squareNum(num){   // here num is argument of function
+    var ans = num*num;
+    return ans;
+}
+
+var square2 = squareNum(n);  // here n is parameter
+console.log(square2);
+var square4 = squareNum(4);  
+console.log(square4);
+ 
