a4935d4aba17f3979f413ec818f7d0d5d6e31729
Anush Rawat <anushra0701@gmail.com>
2023-08-30 23:29:38 +0530

    weaksets and weakmaps

---
+++ Data Types/weakmaps weaksets.js
@@ -0,0 +1,7 @@
+// difference between maps and weakmaps is that weakmaps only take object as key
+// and when reference to that object is not available that object is removed from memory
+
+// weaksets also take objects as values and when reference is not there the object is deleted from memory
+
+
+
