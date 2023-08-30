0e16d317db0e6ddc552dace05d2e045d18b0c500
Anush Rawat <anushra0701@gmail.com>
2023-08-30 21:58:20 +0530

    Numbers

---
+++ Data Types/Numbers.js
@@ -0,0 +1,17 @@
+
+//previous smallest integer
+Math.floor(3.1); // 3 
+Math.floor(-1.1); // -2
+
+
+// next greatest integer
+Math.ceil(2.1);  // 3
+Math.ceil(-1.1);  //-1 
+
+let num = 1.234;
+num.toFixed(2); // 1.23  2 decimal places
+
+// checks if it is a number
+isNaN("str"); // true 
+isFinite("str"); // checks if it finite
+
