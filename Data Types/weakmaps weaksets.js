da9179fbd096775e2199037376b12d8b21433de3
Anush Rawat <anushra0701@gmail.com>
2023-08-30 23:33:42 +0530

    added keys and values extration of object

--- Data Types/weakmaps weaksets.js
+++ Data Types/weakmaps weaksets.js
@@ -4,4 +4,6 @@
 // weaksets also take objects as values and when reference is not there the object is deleted from memory
 
 
-
+// Object.keys(obj) returns an array of keys
+// Object.values(obj) returns an array of values
+// Object.entries(obj) returns an array of [key,value] pairs.
